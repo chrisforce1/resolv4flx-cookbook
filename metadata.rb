d95a97f4c8ad6efe4d4dc4096b45faa94c468dfe
= <=>
2015-05-04 23:57:41 +0200

    start

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'resolv4flxnt'
+maintainer       'pablo mercado'
+maintainer_email 'odacremolbap@gmail.com'
+license          'no license'
+description      'Installs/Configures resolv4flxnt'
+long_description 'Installs/Configures resolv4flxnt'
+version          '0.1.0'
+
